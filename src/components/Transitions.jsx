a75396de82ea24c13d0567a497de6fa5f4197717
PaulManRobertson <bigwallpaul514@gmail.com>
2023-07-26 14:07:19 -0600

    some Animated Circles

---
+++ src/components/Transitions.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const Transitions = () => {
+
+const generateCircles = () => {
+    
+}
+
+const AnimatedCircles = () => {
+    const [visibleCircles, setVisibleCircles] = useState(
+        generateCircles()
+    )
+}
+  return (
+    <div>Transitions</div>
+  )
+}
+
+export default Transitions
